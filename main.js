4c29315798e2ed41b948ab93f5b018c31c0c376d
Thang cg <duongsag.hp11@gmail.com>
2022-05-06 09:50:38 +0700

    Up file area

---
+++ main.js
@@ -0,0 +1,7 @@
+
+let inputWidth = +prompt("Enter the width");
+let inputHeight = +prompt("Enter the height");
+let area = inputWidth * inputHeight;
+document.write("Width:" + inputWidth);
+document.write("<br>height:" + inputHeight);
+document.write("<br>The area is: " + area);
